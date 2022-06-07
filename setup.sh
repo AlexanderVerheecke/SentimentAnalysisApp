d6e2c0936461c4e4468059fc651b1655ca079807
AlexanderVerheecke <93544547+AlexanderVerheecke@users.noreply.github.com>
2022-06-07 16:49:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,8 @@
-# mkdir -p ~/.streamlit/
-
-# echo "\
-# [general]\n\
-# email = \"your-email@domain.com\"\n\
-# " > ~/.streamlit/credentials.toml
-
-# echo "\
-# [server]\n\
-# headless = true\n\
-# enableCORS=false\n\
-# port = $PORT\n\
-# " > ~/.streamlit/config.toml
-
-
 mkdir -p ~/.streamlit/
-
 echo "\
 [server]\n\
+headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-headless = true\n\
 \n\
 " > ~/.streamlit/config.toml

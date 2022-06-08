0ab1cf9e1e7417bb52aa730f8c9ca60b3770e59a
AlexanderVerheecke <93544547+AlexanderVerheecke@users.noreply.github.com>
2022-06-08 14:10:08 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/secrets.toml
+# echo "\
+# [server]\n\
+# headless = true\n\
+# enableCORS=false\n\
+# port = $PORT\n\
+# " > ~/.streamlit/secrets.toml
 

711db1ef37386cdfde865bb549adacbf5aed63c6
AlexanderVerheecke <93544547+AlexanderVerheecke@users.noreply.github.com>
2022-06-08 13:29:44 +0100

    revert setup

--- setup.sh
+++ setup.sh
@@ -6,15 +6,19 @@ email = \"alexander.verheecke@hotmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+<<<<<<< HEAD
 [general]\n\
 email = \"alexander.verheecke@hotmail.com\"\n\
 " > ~/.streamlit/secrets.toml
 
 echo "\
+=======
+>>>>>>> parent of e5b2e39 (secrets setup.sh)
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+<<<<<<< HEAD
 " > ~/.streamlit/config.toml
 
 # echo "\
@@ -23,3 +27,6 @@ port = $PORT\n\
 # enableCORS=false\n\
 # port = $PORT\n\
 # " > ~/.streamlit/secrets.toml
+=======
+" > ~/.streamlit/config.toml
+>>>>>>> parent of e5b2e39 (secrets setup.sh)
